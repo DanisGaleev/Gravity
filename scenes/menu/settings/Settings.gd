a162affde4296b0a3902a08d5cc8e4c8b8bf091a
danis <dgaleev2018@litsey2.ru>
2023-06-23 21:28:45 +0300

    Не доделал перевод

--- scenes/menu/settings/Settings.gd
+++ scenes/menu/settings/Settings.gd
@@ -4,12 +4,17 @@ onready var lang = $MarginContainer/Languages
 
 func _ready():
 	var l = TranslationServer.get_locale()
-	for idx in lang.get_item_count():
-		if lang.get_item_text(idx).to_lower().begins_with(l):
-			lang.select(idx)
-
+	if l == "ru":
+		lang.select(1)
+	elif l == "en":
+		lang.select(0)
 func _on_Languages_item_selected(index):
-	TranslationServer.set_locale(lang.get_item_text(index).to_lower().substr(0, 2))
+	match index:
+		0:
+			TranslationServer.set_locale("en")
+		1:
+			TranslationServer.set_locale("ru")
+	#TranslationServer.set_locale(lang.get_item_text(index).to_lower().substr(0, 2))
 #	save.load_from_file("user://data.txt")
 #	save.add_value("language", lang.get_item_text(index).to_lower())
 #	save.save_to_file("user://data.txt")
